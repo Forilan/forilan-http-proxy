6f6a9d1c17ba113e787899c1fea680d83230b16c
Forilan <1096777077@qq.com>
2021-04-28 15:41:43 +0800

    feat(*): 增加发送后回调处理

--- app/steps/sendUserRes.js
+++ app/steps/sendUserRes.js
@@ -5,7 +5,12 @@ function sendUserRes(Container) {
     if (Container.options.stream) {
       Container.proxy.res.pipe(Container.user.res);
     } else {
-      Container.user.res.send(Container.proxy.resData);
+      let res = Container.user.res.send(Container.proxy.resData);
+      var resolverFn = Container.options.userResSendAfter;
+      if (resolverFn) {
+        resolverFn(Container.user.req, res);
+      }
+
     }
   }
   return Promise.resolve(Container);
